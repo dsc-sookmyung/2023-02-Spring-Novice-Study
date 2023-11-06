e52999cd3ef6c169eea477dbde64c72ed8f0ff8d
Minjeong Kim <kgpg0292@sookmyung.ac.kr>
2023-11-06 19:58:58 +0900

    second commit setting

---
+++ 김민정/freelec-springboot2-webservice/src/test/java/com/jojoldu/book/springboot/HelloControllerTest.java
@@ -0,0 +1,40 @@
+import com.jojoldu.book.springboot.web.HelloController;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class) // 테스트를 진행할 때 JUnit에 내장된 실행자 외에 다른 실행자를 실행시킨다.
+@WebMvcTest(controllers = HelloController.class) // 여러 스프링 테스트 어노테이션 중, Web에 집중할 수 있는 어노테이션
+@ContextConfiguration(classes = HelloController.class) // 클래스 참조
+public class HelloControllerTest {
+
+    @Autowired // 스프링이 관리 하는 빈을 주입 받는다.
+    private MockMvc mvc; // 웹 API를 테스트할 때 사용 한다.
+
+    @Test
+    public void hello가_리턴된다() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello")) // MockMvc를 통해 /hello 주소로 HTTP GET 요청을 한다.
+                .andExpect(status().isOk()) // mvc.perform의 결과를 검증한다.
+                .andExpect(content().string(hello)); // mvc.perform의 결과를 검증한다.
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                get("/hello/dto").param("name", name).param("amount", String.valueOf(amount))).andExpect(status().isOk()).andExpect(jsonPath("$.name", is(name))).andExpect(jsonPath("$.amount", is(amount)));
+    }
+}
