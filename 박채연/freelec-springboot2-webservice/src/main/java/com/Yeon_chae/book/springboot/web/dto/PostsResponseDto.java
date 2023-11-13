1aea9066bfc7159eb6cbba3e4222799dee6fa673
Chaeyeon Park <chay322p@gmail.com>
2023-11-13 23:39:11 +0900

    Feat: 기능 추가

---
+++ 박채연/freelec-springboot2-webservice/src/main/java/com/Yeon_chae/book/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,18 @@
+package com.Yeon_chae.book.springboot.web.dto;
+
+import com.Yeon_chae.book.springboot.domain.posts.Posts;
+
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
--- 박채연/freelec-springboot2-webservice/src/main/java/com/Yeon_chae/book/springboot/web/dto/PostsSaveRequestDto.java
+++ 박채연/freelec-springboot2-webservice/src/main/java/com/Yeon_chae/book/springboot/web/dto/PostsSaveRequestDto.java
@@ -12,12 +12,16 @@ public class PostsSaveRequestDto {
     private String title;
     private String content;
     private String author;
+
     @Builder
-    public PostsSaveRequestsDto(String title, String content, String author) {
+    public PostsSaveRequestDto(String title, String content, String author) {
         this.title = title;
         this.content = content;
         this.author = author;
     }
+
+    @Builder
+
 
     public Posts toEntity() {
         return Posts.builder()
