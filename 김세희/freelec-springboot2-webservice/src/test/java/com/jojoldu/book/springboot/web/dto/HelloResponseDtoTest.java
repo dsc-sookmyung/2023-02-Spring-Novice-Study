c3a150a272cff6e402898bbbc2b2e8b5f407e660
김세희 <lolry0207@gmail.com>
2023-11-15 16:11:22 +0900

    2주차 - (3) :: 1주차 코드 수정 반영

--- 김세희/freelec-springboot2-webservice/src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
+++ 김세희/freelec-springboot2-webservice/src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 public class HelloResponseDtoTest {
 
+    @Test
     public void 롬복_기능_테스트(){
         String name = "test";
         int amount =  1000;
