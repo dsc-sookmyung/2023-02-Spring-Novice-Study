f77aea52d2bc5ec35faa75a756b8f630fa4fb314
GDSCMJ <kgpg0292@sookmyung.ac.kr>
2023-11-18 00:52:27 +0900

    Update PostsListResponseDto.java

--- 김민정/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/web/dto/PostsListResponseDto.java
+++ 김민정/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/web/dto/PostsListResponseDto.java
@@ -5,13 +5,14 @@ import com.jojoldu.book.springboot.domain.posts.Posts;
 import java.time.LocalDateTime;
 
 public class PostsListResponseDto {
+    
     private Long id;
     private String title;
     private String author;
     private LocalDateTime modifiedDate;
 
     public PostsListResponseDto(Posts entity) {
-        this.id = entity.getId();
+        Long id = entity.getId();
         this.title = entity.getTitle();
         this.author = entity.getAuthor();
         this.modifiedDate = entity.getModifiedDate();
