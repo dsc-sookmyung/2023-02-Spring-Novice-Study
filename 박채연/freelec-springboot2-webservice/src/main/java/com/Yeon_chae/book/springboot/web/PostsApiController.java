ded9b5da2d4fc055f977a81000dc4d118582d78a
Chaeyeon Park <chay322p@gmail.com>
2023-11-20 23:57:46 +0900

    Feat: [3주차] @RequestMapping -> 중복코드 제거

--- 박채연/freelec-springboot2-webservice/src/main/java/com/Yeon_chae/book/springboot/web/PostsApiController.java
+++ 박채연/freelec-springboot2-webservice/src/main/java/com/Yeon_chae/book/springboot/web/PostsApiController.java
@@ -1,36 +1,42 @@
 package com.Yeon_chae.book.springboot.web;
 
 import com.Yeon_chae.book.springboot.service.posts.PostsService;
+import com.Yeon_chae.book.springboot.web.dto.PostsListResponseDto;
 import com.Yeon_chae.book.springboot.web.dto.PostsResponseDto;
 import com.Yeon_chae.book.springboot.web.dto.PostsSaveRequestDto;
 import com.Yeon_chae.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.List;
+
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/posts")
 @RestController
 public class PostsApiController {
 
     private final PostsService postsService;
 
-    @PostMapping("/api/v1/posts")
-    public Long save(@RequestBody PostsSaveRequestDto
-                                            requestDto) {
+    @PostMapping
+    public Long save(@RequestBody @Valid PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
     }
-    @PutMapping("/api/v1/posts/{id}")
+
+    @GetMapping
+    public List<PostsListResponseDto> getPostsList(){
+        return postsService.findAllDesc();
+    }
+
+    @PutMapping("/{id}")
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
         return postsService.update(id, requestDto);
     }
 
-    @GetMapping("/api/v1/posts/{id}")
-    public PostsResponseDto findById (@PathVariable Long id) {
-        return postsService.findById(id);
-    }
-
-    @DeleteMapping("/api/v1/posts/{id}")
+    @DeleteMapping("/{id}")
     public Long delete(@PathVariable Long id) {
         postsService.delete(id);
         return id;
     }
+
 }
