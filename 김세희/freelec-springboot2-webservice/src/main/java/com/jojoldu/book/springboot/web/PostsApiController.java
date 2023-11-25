100ab08674980154f2ba05ce8a25a3d40e94bde5
김세희 <lolry0207@gmail.com>
2023-11-25 20:44:25 +0900

    4주차-(1) (5.1 ~ 5.6)

--- 김세희/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
+++ 김세희/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
@@ -1,8 +1,7 @@
 package com.jojoldu.book.springboot.web;
 
-import com.jojoldu.book.springboot.service.posts.PostsService;
+import com.jojoldu.book.springboot.service.PostsService;
 import com.jojoldu.book.springboot.web.dto.PostsListResponseDto;
-import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
