2871cacdec71c1c635b2161f892d1a593936e1ba
jiwonniy <jiwoni0707@gmail.com>
2023-11-26 18:25:09 +0900

    Feat : [4주차] @EnableJpaAuditing 제거

--- 우지원/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/Application.java
+++ 우지원/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/Application.java
@@ -3,10 +3,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+//@EnableJpaAuditing가 삭제됨
 @SpringBootApplication
 public class Application {
     public static void main(String[] args){
+
         SpringApplication.run(Application.class, args);
     }
 }
