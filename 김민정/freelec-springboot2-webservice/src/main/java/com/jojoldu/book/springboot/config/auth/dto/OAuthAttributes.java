d761fd34b4f0d6ac5bc03b4763021e30bcd07ae8
GDSCMJ <kgpg0292@sookmyung.ac.kr>
2023-11-27 21:10:41 +0900

    Update : 네이버 로그인

--- 김민정/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/config/auth/dto/OAuthAttributes.java
+++ 김민정/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -17,6 +17,7 @@ public class OAuthAttributes {
 
     @Builder
     public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture) {
+        this.attributes = attributes;
         this.nameAttributeKey = nameAttributeKey;
         this.name = name;
         this.email = email;
@@ -24,11 +25,19 @@ public class OAuthAttributes {
     }
 
     public  static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if ("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
+
         return ofGoogle(userNameAttributeName, attributes);
     }
 
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
         return OAuthAttributes.builder().name((String) attributes.get("name")).email((String) attributes.get("email")).picture((String) attributes.get("picture")).attributes(attributes).nameAttributeKey(userNameAttributeName).build();
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        return OAuthAttributes.builder().name((String) response.get("name")).email((String) response.get("email")).picture((String) response.get("picture")).attributes(response).nameAttributeKey(userNameAttributeName).build();
     }
 
     public User toEntity() {
