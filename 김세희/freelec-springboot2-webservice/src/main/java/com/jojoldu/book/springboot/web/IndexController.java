ae771d488851f4ee12a15e66c27450548ae5a72e
5jisoo <56earls@gmail.com>
2023-12-01 01:21:08 +0900

    Revert "remove source files"
    This reverts commit 833752c9f4e4b5f72a1410aad112d23a1e00c7c8.

--- 김세희/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ 김세희/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,4 +1,5 @@
 package com.jojoldu.book.springboot.web;
+
 import com.jojoldu.book.springboot.config.auth.LoginUser;
 import com.jojoldu.book.springboot.config.auth.dto.SessionUser;
 import com.jojoldu.book.springboot.service.PostsService;
@@ -8,17 +9,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
-
         if(user != null){
             model.addAttribute("userName", user.getName());
         }
