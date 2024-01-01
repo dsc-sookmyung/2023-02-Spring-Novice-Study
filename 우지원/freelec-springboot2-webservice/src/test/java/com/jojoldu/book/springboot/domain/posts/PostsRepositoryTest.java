8719008d0ac77d629cef3234a2657caeed06be3c
jiwonniy <jiwoni0707@gmail.com>
2024-01-02 02:12:39 +0900

    Test : [6주차] 어노테이션 수정(2)

--- 우지원/freelec-springboot2-webservice/src/test/java/com/jojoldu/book/springboot/domain/posts/PostsRepositoryTest.java
+++ 우지원/freelec-springboot2-webservice/src/test/java/com/jojoldu/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = Application.class)
+@SpringBootTest
 public class PostsRepositoryTest {
 
     @Autowired
